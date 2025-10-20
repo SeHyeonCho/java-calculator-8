31b829fd692d0719184aa7d8ab7fbc6ba7ac7f26
sehyeon <Eugene8500@gmail.com>
2025-10-20 17:29:18 +0900

    feat(calculator): implement custom delimiter support

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,34 @@
 package calculator;
 
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        System.out.println("덧셈할 문자열을 입력해 주세요.");
+        String input = readLine();
+
+        List<Character> delimiters = new ArrayList<>(List.of(',', ':'));
+        int addedCount = addDelimiters(input, delimiters);
+    }
+
+    private static int addDelimiters(String input, List<Character> delimiters) {
+        Pattern pattern = Pattern.compile("//(.*?)\\\\n");
+        Matcher matcher = pattern.matcher(input);
+        int addedCount = 0;
+
+        while (matcher.find()) {
+            String delimiterText = matcher.group(1).trim();
+            for (char delimiter : delimiterText.toCharArray()) {
+                delimiters.add(delimiter);
+                addedCount++;
+            }
+        }
+
+        return addedCount;
     }
 }
