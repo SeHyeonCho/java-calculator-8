0ecd292158ab695bb471294121a2634ca070ed53
sehyeon <Eugene8500@gmail.com>
2025-10-20 19:38:01 +0900

    feat(calculator): implement number parsing and summing logic

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -13,7 +13,35 @@ public class Application {
         String input = readLine();
 
         List<Character> delimiters = new ArrayList<>(List.of(',', ':'));
+
+        int sum = 0;
         int addedCount = addDelimiters(input, delimiters);
+
+        if (addedCount > 0) {
+            input = input.substring(addedCount + 4);
+        }
+
+        String[] numbers = input.split(buildRegex(delimiters));
+        for (String number : numbers) {
+            try {
+                sum += Integer.parseInt(number);
+            } catch (NumberFormatException e) {
+                throw new IllegalArgumentException(("잘못된 입력 값 입니다."));
+            }
+        }
+        System.out.println("결과 : " + sum);
+    }
+
+    private static String buildRegex(List<Character> delimiters) {
+        StringBuilder sb = new StringBuilder("[");
+        for (char ch : delimiters) {
+            if ("\\.^$|?*+()[]{}".indexOf(ch) != -1) {
+                sb.append("\\");
+            }
+            sb.append(ch);
+        }
+        sb.append("]");
+        return sb.toString();
     }
 
     private static int addDelimiters(String input, List<Character> delimiters) {
